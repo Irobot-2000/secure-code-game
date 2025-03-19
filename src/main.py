a6e0714205c91b89e02a6d7b4bff678d6693ec90
adeeshajayasinghe <jayasingheai.20@uom.lk>
2025-03-19 21:27:42 +0530

    SCG:1.0.0:NA #dev Added initial vulnerable code

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from collections import namedtuple
+
+Order = namedtuple("Order", "id, items")
+Item = namedtuple("Item", "type, description, amount, quantity")
+
+
+def validorder(order: Order):
+    net = 0
+
+    for item in order.items:
+        if item.type == "payment":
+            net += item.amount
+        elif item.type == "product":
+            net -= item.amount * item.quantity
+        else:
+            return "Invalid item type: %s" % item.type
+
+    if net != 0:
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
+    else:
+        return "Order ID: %s - Full payment received!" % order.id
